2aa1665cd47fb35a46e80fb5b131f1b6bebad981
nevada_scout <travis@impactmod.com>
2015-10-25 00:48:14 +0100

    Added DisplayHelpers

---
+++ Codex.Web/Helpers/DisplayHelpers.cs
@@ -0,0 +1,29 @@
+﻿namespace Codex.Web.Helpers
+{
+    using System.Web.Mvc;
+
+    using Codex.Persistence.Repositories;
+    using Codex.Security;
+
+    public class DisplayHelpers
+    {
+        public static MvcHtmlString GetGravatarForUser(string email, int size = 50)
+        {
+            var emailHash = Md5.CreateHash(email);
+            return new MvcHtmlString($"https://secure.gravatar.com/avatar/{emailHash}?s={size}");
+        }
+
+        //public static MvcHtmlString GetAvatarForUser(string email)
+        //{
+        //    var accountDb = new UsersRepository();
+        //    var user = accountDb.GetUserByEmail(email);
+
+        //    if (user.Avatar == Avatar.Gravatar)
+        //    {
+        //        return GetGravatarForUser(email);
+        //    }
+
+        //    return new MvcHtmlString("/Content/images/avatars/" + user.Avatar + ".png");
+        //}
+    }
+}
